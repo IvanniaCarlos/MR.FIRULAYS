642b313f48b958300c98757d8baafd26bbe542be
Your Name <ivannid.n.c@gmail.com>
2026-05-24 02:46:43 -0500

    Dia

---
+++ conexion.php
@@ -0,0 +1,10 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "mrfirulays";
+$conexion = mysqli_connect($host, $user, $pass, $db);
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
+?>
