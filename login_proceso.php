07c67ef36c22420518f76cb1eda1e6e2875a88e4
Your Name <ivannid.n.c@gmail.com>
2026-05-29 01:11:38 -0500

    Di

--- login_proceso.php
+++ login_proceso.php
@@ -1,8 +1,8 @@
 <?php
-include 'conexion.php';
 session_start();
 
-// Limpiamos los datos para evitar inyecciones básicas
+include 'conexion.php';
+
 $correo   = mysqli_real_escape_string($conexion, $_POST['correo']);
 $password = $_POST['password'];
 
@@ -13,8 +13,11 @@ $usuario = mysqli_fetch_assoc($resultado);
 if ($usuario && password_verify($password, $usuario['password'])) {
     $_SESSION['usuario'] = $usuario['correo'];
     $_SESSION['nombre_usuario'] = $usuario['nombre'];
+
+    $_SESSION['id_usuario'] = $usuario['id'];
+
     session_write_close();
-    
+
     header("Location: principal.php");
     exit();
 } else {
